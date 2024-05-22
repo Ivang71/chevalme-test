add441c58cde62f2e0d962ccdc459790a09c9770
Ivang71 <artemtolv@gmail.com>
2024-05-22 19:31:13 +0500

    position buttons

--- app/components/ScrollingSections.tsx
+++ app/components/ScrollingSections.tsx
@@ -52,19 +52,21 @@ const ScrollingSections = () => {
                         <span className={`${s.projects} ${s.projActive} mr-[89px]`}>Ready</span>
                         <span className={`${s.projects}`}>New Projects</span>
                     </div>
-                    <div className='mt-[28px] flex text-sm'>
+                    <div className='mt-[15px] ml-[-13px] flex text-sm'>
                         <button className='py-3 pl-2 grid place-items-center grid-cols-2 gap-1 rounded-3xl hover:bg-white/30 active:bg-white/60 hover:text-gray-700 active:text-black transition duration-200 ease-in-out'>
                             Buy
-                            <Image src='/chevalme-test/icons/expand_more.svg' alt='Arrow down' width={32} height={32} className='ml-[-10px] mr-[15px]'/>
+                            <Image src='/chevalme-test/icons/expand_more.svg' alt='Arrow down' width={32} height={32} className='ml-[-10px] mr-[12px]' />
                         </button>
-                        <button className='mr-[13px] p-3 rounded-3xl hover:bg-white/30 active:bg-white/60 hover:text-gray-700 active:text-black transition duration-200 ease-in-out'>Community or Building</button>
+                        <button className='mr-[11px] p-3 rounded-3xl hover:bg-white/30 active:bg-white/60 hover:text-gray-700 active:text-black transition duration-200 ease-in-out'>Community or Building</button>
                         <div className={`${s.line}`} />
-                        <button className='mx-[19px] p-3 rounded-3xl hover:bg-white/30 active:bg-white/60 hover:text-gray-700 active:text-black transition duration-200 ease-in-out'>Location</button>
+                        <button className='mx-[16px] p-3 rounded-3xl hover:bg-white/30 active:bg-white/60 hover:text-gray-700 active:text-black transition duration-200 ease-in-out'>Location</button>
                         <div className={`${s.line}`} />
-                        <button className='mx-[21px] p-3 rounded-3xl hover:bg-white/30 active:bg-white/60 hover:text-gray-700 active:text-black transition duration-200 ease-in-out'>Price</button>
+                        <button className='mx-[18px] p-3 rounded-3xl hover:bg-white/30 active:bg-white/60 hover:text-gray-700 active:text-black transition duration-200 ease-in-out'>Price</button>
                         <div className={`${s.line}`} />
-                        <button className='ml-[22px] mr-[4px] p-3 rounded-3xl hover:bg-white/30 active:bg-white/60 hover:text-gray-700 active:text-black transition duration-200 ease-in-out'>Search</button>
-                        <Image src='/chevalme-test/icons/search.svg' alt='Search icon' width={17} height={17} className='mr-[20px] mt-[-8px]' />
+                        <button className='ml-3 py-3 pl-2 grid place-items-center grid-cols-2 rounded-3xl hover:bg-white/30 active:bg-white/60 hover:text-gray-700 active:text-black transition duration-200 ease-in-out'>
+                            Search
+                            <Image src='/chevalme-test/icons/search.svg' alt='Search icon' width={17} height={17} className='ml-[-20px]' />
+                        </button>
                     </div>
                 </div>
 
