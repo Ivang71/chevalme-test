8eeae8e018032a1ba3cf188f2226e0f1353cf29b
Ivang71 <artemtolv@gmail.com>
2024-05-29 09:48:59 +0500

    fix header active tabs

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -6,16 +6,22 @@ import { usePathname } from 'next/navigation'
 import styles from '@/app/ui/home.module.css'
 
 const routes = ['/', '/about', '/projects/ready', '/services', '/contact']
-const routesMapping: { [k: string]: string } = {
+const routeNameMapping: { [k: string]: string } = {
     '/': 'Home',
     '/about': 'About',
     '/projects/ready': 'Properties',
     '/services': 'Services',
-    '/contact': 'Contact'
+    '/contact': 'Contact',
+}
+const isRouteActive = (route: string, path: string): boolean => {
+    if (path === '/' && route === '/') return true
+    if (route !== '/' && path.includes(route)) return true
+    if (route === '/projects/ready' && path.includes('project')) return true
+    return false
 }
 
 export const Header = () => {
-    const pathname = usePathname()
+    const path = usePathname()
 
     return (
         <div className={`mb-[50px] sm:mb-0 px-[3vw] sm:px-[85px] h-fit sm:h-[340px] pt-[89px]`}>
@@ -25,10 +31,9 @@ export const Header = () => {
             <nav className='mt-[41px] flex flex-wrap gap-[1px] text-sm'>
                 {routes.map((route) => (
                     <Link key={route} href={route + '/'}>
-                        <div
-                            className={`${pathname === route ? styles.navActive : ''} cursor-pointer p-4 rounded-xl hover:bg-white/30 active:bg-white/60 hover:text-gray-900 active:text-black transition duration-200 ease-in-out`}
-                        >
-                            <span>{routesMapping[route]}</span>
+                        <div className={`${isRouteActive(route, path) ? styles.navActive : ''}
+                            cursor-pointer p-4 rounded-xl hover:bg-white/30 active:bg-white/60 hover:text-gray-900 active:text-black transition duration-200 ease-in-out`}>
+                            <span>{routeNameMapping[route]}</span>
                         </div>
                     </Link>
                 ))}
