f7c3bf8b673e56f6b74d522778a2271bcfeab7de
Ivang71 <artemtolv@gmail.com>
2024-06-13 11:40:19 +0500

    fix env

--- app/constants.tsx
+++ app/constants.tsx
@@ -1,3 +1,5 @@
-export const apiUrl = process.env.NEXT_PUBLIC_API_URL
+// export const apiUrl = process.env.NEXT_PUBLIC_API_URL
+export const apiUrl = 'https://codeandcode.xyz/demo/slim_properties'
 export const fetcher = (url: string) => fetch(url).then((res) => res.json())
-export const gmapsApiKey = process.env.NEXT_PUBLIC_GMAPS_API_KEY as string
+// export const gmapsApiKey = process.env.NEXT_PUBLIC_GMAPS_API_KEY as string
+export const gmapsApiKey = 'AIzaSyAS49hXHr0hePqJdzalBGh3iTOuDrgXX00'
