6629339c6e0a6b62239c8b234e7b3f445efd3b0a
Ivang71 <artemtolv@gmail.com>
2024-06-27 10:05:28 +0500

    update gmaps api key

--- app/constants.tsx
+++ app/constants.tsx
@@ -2,4 +2,4 @@
 export const apiUrl = 'https://codeandcode.xyz/demo/slim_properties'
 export const fetcher = (...args: any) => fetch(args).then((res) => res.json())
 // export const gmapsApiKey = process.env.NEXT_PUBLIC_GMAPS_API_KEY as string
-export const gmapsApiKey = 'AIzaSyAS49hXHr0hePqJdzalBGh3iTOuDrgXX00'
+export const gmapsApiKey = 'AIzaSyCDa5c5aPsyG7em9u83ZNv2Jehc20saWZk'
